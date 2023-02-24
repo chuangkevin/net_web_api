90d9ef111c68e3d73b89afe888340318cb3b33c6
Kevin <kevin950805@gmail.com>
2023-02-25 06:57:34 +0800

    Complete controller activities for CRUD.

--- src/WebAPI/WebAPI/APIControllers/DatabaseApi.cs
+++ src/WebAPI/WebAPI/APIControllers/DatabaseApi.cs
@@ -14,41 +14,84 @@ namespace WebAPI.APIControllers
     public class DatabaseApi : ControllerBase
     {
 
+        /// <summary>
+        /// Return all customers.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IEnumerable<CCUSTOMER>> GetCustomers([FromServices] ICustomerService customerService)
         {
             return await customerService.GetCustomers();
         }
 
+        /// <summary>
+        /// Return specific customer by SN.
+        /// </summary>
+        /// <param name="customerSN"></param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<CCUSTOMER> GetCustomersBySN([FromServices] ICustomerService customerService, Int64 customerSN)
         {
             return await customerService.GetCustomerBySN(customerSN);
         }
+
+        /// <summary>
+        /// Create customer with name and phone number.
+        /// </summary>
+        /// <param name="customerService"></param>
+        /// <param name="requestParam"></param>
+        /// <param name="NAME"></param>
+        /// <param name="PHONE_NO"></param>
+        ///  /// <remarks>
+        /// Sample request:
+        ///
+        ///     {
+        ///        "NAME": "Kevin",
+        ///        "PHONE_NO": "0912345678"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns></returns>
         [HttpPost]
-        public string CreateCustomer([FromBody] JsonElement requestParam)
+        public async Task<Int64> CreateCustomer([FromServices] ICustomerService customerService, [FromBody] JsonElement requestParam)
         {
-            string ret = "";
+            CCUSTOMER toCreate = new CCUSTOMER(requestParam.ToString());
 
-            CCUSTOMER toCreate = new CCUSTOMER();
-
-            return ret;
+            return await customerService.SaveCustomer(toCreate);
         }
 
+        /// <summary>
+        /// Update specific customer's info by SN.
+        /// </summary>
+        ///  /// <remarks>
+        /// Sample request:
+        ///
+        ///     {
+        ///        "SN":18,
+        ///        "NAME": "Kevin",
+        ///        "PHONE_NO": "0912345678"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns></returns>
         [HttpPut]
-        public string UpdateCustomer()
+        public async Task<Int64> UpdateCustomer([FromServices] ICustomerService customerService,[FromBody] JsonElement requestParam)
         {
-            string ret = "";
+            CCUSTOMER toCreate = new CCUSTOMER(requestParam.ToString());
+            
 
-            return ret;
+            return await customerService.SaveCustomer(toCreate);
         }
 
+        /// <summary>
+        /// Delete specific customer by SN.
+        /// </summary>
+        /// <param name="customerSN"></param>
+        /// <returns></returns>
         [HttpDelete]
-        public string DeleteCustomer()
+        public async Task<bool> DeleteCustomer([FromServices] ICustomerService customerService, Int64 customerSN)
         {
-            string ret = "";
-
-            return ret;
+            return await customerService.DeleteCustomer_SET_STATE(customerSN);
         }
     }
 }
