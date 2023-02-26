86c5c512938eb71764118ab4b1c348445f29af3f
Kevin <kevin950805@gmail.com>
2023-02-27 02:55:18 +0800

    Change the data communication of the page to use Web API.

--- src/WebAPI/WebAPI/Pages/CustomerMgr.razor.cs
+++ src/WebAPI/WebAPI/Pages/CustomerMgr.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using WebAPI.IServices;
+using WebAPI.Services;
 using static WebApi.Data.MsSQLDataAccess;
 
 namespace WebAPI.Pages
@@ -7,51 +8,60 @@ namespace WebAPI.Pages
     public partial class CustomerMgr
     {
         public IEnumerable<CCUSTOMER> ForBind_CustomerEntries;
+        //[Inject]
+        //public ICustomerService CustomerServices { get; set; }
+        public bool SeeOnlyValid { get; set; }
+
         [Inject]
-        public ICustomerService CustomerServices { get; set; }
+        public IApiService ApiCustomerServices { get; set; }
 
         public CCUSTOMER ForBind_Customer = new CCUSTOMER();
         protected override async Task OnInitializedAsync()
         {
             await this.GetCustomers();
         }
+
+
         protected async Task GetCustomers()
         {
-            ForBind_CustomerEntries = await CustomerServices.GetCustomers_Alive();
+            if (SeeOnlyValid)
+                ForBind_CustomerEntries = await ApiCustomerServices.GetCustomers_Alive();
+            else
+                ForBind_CustomerEntries = await ApiCustomerServices.GetCustomers();
         }
+
         protected async Task SaveCustomer()
         {
-            await CustomerServices.SaveCustomer(ForBind_Customer);
+            await ApiCustomerServices.CreateCustomer(ForBind_Customer);
             await this.GetCustomers();
             this.ClearAll();
             StateHasChanged();
         }
         protected async Task UpdateCustomer()
         {
-            await CustomerServices.SaveCustomer(ForBind_Customer);
+            await ApiCustomerServices.UpdateCustomer(ForBind_Customer);
             await this.GetCustomers();
             this.ClearAll();
         }
         protected async Task GetCustomerBySN(Int64 customerSN)
         {
-            ForBind_Customer = await CustomerServices.GetCustomerBySN(customerSN);
+            ForBind_Customer = await ApiCustomerServices.GetCustomerBySN(customerSN);
             ForBind_Customer.IsUpdate = true;
             await this.GetCustomers();
         }
         protected async Task DeleteSales_SetState(Int64 customerSN)
         {
-            await CustomerServices.DeleteCustomer_SET_STATE(customerSN);
+            await ApiCustomerServices.DeleteCustomer(customerSN);
             await this.GetCustomers();
         }
 
         protected async Task DeleteSales_DeleteAlways(Int64 customerSN)
         {
-            await CustomerServices.DeleteCustomer_SQL_DELETE(customerSN);
+            await ApiCustomerServices.DeleteCustomer(customerSN);
             await this.GetCustomers();
         }
         public void ClearAll()
         {
-          
             ForBind_Customer = new CCUSTOMER();
         }
     }
